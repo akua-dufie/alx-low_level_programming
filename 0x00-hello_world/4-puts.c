a683917bd9294440dfa46d3b02eebb51a001ebc5
Akua Dufie <abigailappiahkubi18@gmail.com>
2023-02-16 14:42:13 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- * main - prints a line of code with puts
+ * main - prints a line of code with puts function
  *
  * Return: 0
 */
-int main(void)
+int main(void);
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
