53026fe7d4726e3bba0d4cb04cbaaedb498c34c9
Akua Dufie <abigailappiahkubi18@gmail.com>
2023-02-16 16:22:00 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - using fput to  print a line to the standard error
+ * main - fputs to  print a line to the standard error
  *
  * Return: 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
