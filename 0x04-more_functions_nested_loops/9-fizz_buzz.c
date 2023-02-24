3f6454ca7dc2a17299303f15ac6bfd930796f108
Akua Dufie <abigailappiahkubi18@gmail.com>
2023-02-23 17:09:13 -0800

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,21 +15,20 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz");
+			printf("Fizz\t");
 		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz");
+			printf("Buzz\t");
 		} else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz\t");
 		} else if (i == 1)
 		{
-			printf("%d", i);
+			printf("%d\t", i);
 		} else
 		{
-			printf("%d", i);
+			printf("%d\t", i);
 		}
-		printf(" ")
 	}
 	printf("\n");
 
