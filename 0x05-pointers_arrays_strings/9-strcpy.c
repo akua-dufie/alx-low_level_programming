3fa498d2dd8c53ad667df45ef87b7036bf10787d
Akua Dufie <abigailappiahkubi18@gmail.com>
2023-02-27 14:27:41 -0800

    Update

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * char*_strcpy - a function that copies the string pointed to br src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+
+	dest[a] = '\0';
+
+	return (dest);
+}
