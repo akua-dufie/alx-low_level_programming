4da146a4530ae9b6b2627aca960df9da6ff40847
Akua Dufie <abigailappiahkubi18@gmail.com>
2023-02-27 14:48:35 -0800

    Update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ void puts_half(char *str)
 	int a, n, longi;
 
 	longi = 0;
-	for (a = 0; str[a] != '\0', a++)
+	for (a = 0; str[a] != '\0'; a++)
 		longi++;
 
 	n = (longi / 2);
@@ -20,7 +20,7 @@ void puts_half(char *str)
 	if ((longi % 2) == 1)
 		n = ((longi + 1) / 2);
 
-	for (a = n; str[a] != '\0', a++)
+	for (a = n; str[a] != '\0'; a++)
 		_putchar(str[a]);
 	_putchar('\n');
 }
