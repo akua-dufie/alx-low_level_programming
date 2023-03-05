67008ce4a665387ada97664f792c2a02d690853b
Akua Dufie <abigailappiahkubi18@gmail.com>
2023-03-05 15:06:09 -0800

    Update

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ *
+ * @c: occurance of the character
+ * @s: in the string
+ *
+ * Return: a pointer to the first occurance of the character
+ */
+
+char *_strchr(char *s, char *c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
